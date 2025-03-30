1553367346f8a3a5ff9091eeab4bf1544602c768
Divyansh chaudhary <hypr123890@gmail.com>
2025-03-30 09:59:02 +0530

    backend question model done

---
+++ Backend/src/models/question.model.js
@@ -0,0 +1,37 @@
+
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        required: [true, "Title is required"],
+        minLength: [5, "Title should have at least 5 characters"],
+        maxLength: [50, "Title is too long"]
+    },
+    body: {
+        type: String,
+        maxLength: [300, "Question is too long, Make it short"]
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    authorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    votes: {
+        type: Number,
+        default: 0
+    },
+    answersCount: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+const questionModel = mongoose.model("Question", questionSchema);
+
+module.exports = questionModel;
